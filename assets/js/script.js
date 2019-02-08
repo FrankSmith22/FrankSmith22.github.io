25c3a219c4167e05c926d18cb211c108730646bc
FrankSmith22 <fasvi6@gmail.com>
2019-02-08 16:16:33 +0000

    removed obnoxious parallax effect, added contact form (no functionality just yet), small visual changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,4 @@ function initialize(){
 	//initialize SCROLLSPY
     var scrollSpyElems = document.querySelectorAll('.scrollspy');
     var scrollSpyInstances = M.ScrollSpy.init(scrollSpyElems);
-    //initialize PARALLAX
-    var parallaxElems = document.querySelectorAll('.parallax');
-    var parallaxInstances = M.Parallax.init(parallaxElems);
 }
