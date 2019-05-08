ff1659c4a5ba959545e792c3fffdf7efbacda93c
franksmith22 <fasvi6@gmail.com>
2019-05-07 20:08:45 -0700

    added some fancy css animations to my short about me and picture on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,25 @@ function initialize(){
 	//initialize SCROLLSPY
     var scrollSpyElems = document.querySelectorAll('.scrollspy');
     var scrollSpyInstances = M.ScrollSpy.init(scrollSpyElems);
+    
+    introduction();
+}
+
+function introduction(){
+	slideMeHolder();
+	fadeInText();
+}
+
+function slideMeHolder(){
+	let meHolder = document.querySelector('#meHolder');
+	
+	meHolder.style.transform = "translate(-50%, -50%) rotate(45deg)";
+}
+
+function fadeInText(){
+	let frankHeader = document.querySelector('#frankHeader');
+	let aboutMeHolder = document.querySelector('#aboutMeHolder');
+	
+	frankHeader.style.opacity = '1';
+	aboutMeHolder.style.opacity = '1';
 }
